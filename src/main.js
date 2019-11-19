1a8ef1ea56b7173ea29571d6afcefff1227b0442
rainyan <rainyan@tencent.com>
2019-11-19 20:17:59 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 导入组件库
+import vueCanvasPoster from 'vue-canvas-poster'
+// 注册组件库
+Vue.use(vueCanvasPoster)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
