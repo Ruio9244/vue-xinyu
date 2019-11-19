ae9248c82b7d125020ae0924353e0957c56588a4
rainyan <rainyan@tencent.com>
2019-11-19 20:33:57 +0800

    提交router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 导入组件库
+import VueRouter from 'vue-router'
 import vueCanvasPoster from 'vue-canvas-poster'
+
+Vue.use(VueRouter)
 // 注册组件库
 Vue.use(vueCanvasPoster)
 Vue.config.productionTip = false
---
+++ src/page/certification/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "certification"
+    }
+</script>
+
+<style scoped>
+
+</style>
